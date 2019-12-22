8a19f51b6ef42d8b03fce333169f217abff33717
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2019-12-22 17:11:29 +0200

    Add example (#3)
    * added example
    
    * made test from example
    
    * removed redundant
    
    * removed redundant from docstring
    
    * renamed

---
+++ example_test.go
@@ -0,0 +1,27 @@
+package epoch_test
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/vtopc/epoch"
+)
+
+type Request struct {
+	Timestamp epoch.Seconds `json:"timestamp"`
+}
+
+func ExampleSeconds() {
+	var v Request
+	err := json.Unmarshal([]byte(`{"timestamp":1136239445}`), &v)
+	if err != nil {
+		panic(err)
+	}
+
+	// Also as epoch.Seconds inherits all time.Time's methods:
+	fmt.Println(v.Timestamp.Year())
+	fmt.Println(v.Timestamp.UTC().String())
+	// Output:
+	// 2006
+	// 2006-01-02 22:04:05 +0000 UTC
+}
